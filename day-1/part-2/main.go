dd2d855e4f7fd1ca7de2139bffb14230cf65a449
Benjamin Ruston <benjy.ruston@gmail.com>
2018-12-09 19:22:51 +0000

    Add day 1 part 2 solution

---
+++ day-1/part-2/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	b, err := ioutil.ReadFile("input")
+	if err != nil {
+		log.Fatalf("unable to open input file: %v", err)
+	}
+	sum := 0
+	seen := make(map[int]struct{})
+	for {
+		s := bufio.NewScanner(bytes.NewReader(b))
+		for s.Scan() {
+			n := 0
+			fmt.Sscanf(s.Text(), "%d", &n)
+			if _, err := fmt.Sscanf(s.Text(), "%d", &n); err != nil {
+				log.Fatalf("error while scanning input: %s", err)
+			}
+			sum += n
+			if _, ok := seen[sum]; ok {
+				fmt.Println(sum)
+				return
+			}
+			seen[sum] = struct{}{}
+		}
+		if err := s.Err(); err != nil {
+			log.Fatal("error reading from input: %s", err)
+		}
+	}
+}
